e07950d390caded130acc3426d501547d03f17bf
myranoor <mnoor.devgenius@gmail.com>
2024-08-22 14:42:14 +0400

    installed clerk

--- src/components/Footer/Header/Header.jsx
+++ src/components/Footer/Header/Header.jsx
@@ -1,29 +1,29 @@
-"use-client";
-import React, from  "react";
+"use client";
+import React from 'react';
 
 export default function Header() {
   return (
-    <header class="flex shadow-md py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
-      <div class="flex flex-wrap items-center justify-between gap-5 w-full">
+    <header className="flex shadow-md py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
+      <div className="flex flex-wrap items-center justify-between gap-5 w-full">
         <a href="javascript:void(0)">
           <img
             src="https://readymadeui.com/readymadeui.svg"
             alt="logo"
-            class="w-36"
+            className="w-36"
           />
         </a>
 
         <div
           id="collapseMenu"
-          class="max-lg:hidden lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50"
+          className="max-lg:hidden lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50"
         >
           <button
             id="toggleClose"
-            class="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3"
+            className="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-4 fill-black"
+              className="w-4 fill-black"
               viewBox="0 0 320.591 320.591"
             >
               <path
@@ -37,60 +37,60 @@ export default function Header() {
             </svg>
           </button>
 
-          <ul class="lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
-            <li class="mb-6 hidden max-lg:block">
+          <ul className="lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
+            <li className="mb-6 hidden max-lg:block">
               <a href="javascript:void(0)">
                 <img
                   src="https://readymadeui.com/readymadeui.svg"
                   alt="logo"
-                  class="w-36"
+                  className="w-36"
                 />
               </a>
             </li>
-            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                class="hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]"
               >
                 Home
               </a>
             </li>
-            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
               >
                 Team
               </a>
             </li>
-            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
               >
                 Feature
               </a>
             </li>
-            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
               >
                 Blog
               </a>
             </li>
-            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
               >
                 About
               </a>
             </li>
-            <li class="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
+            <li className="max-lg:border-b border-gray-300 max-lg:py-3 px-3">
               <a
                 href="javascript:void(0)"
-                class="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+                className="hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
               >
                 Contact
               </a>
@@ -98,25 +98,25 @@ export default function Header() {
           </ul>
         </div>
 
-        <div class="flex max-lg:ml-auto space-x-3">
-          <button class="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+        <div className="flex max-lg:ml-auto space-x-3">
+          <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
             Login
           </button>
-          <button class="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
+          <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]">
             Sign up
           </button>
 
-          <button id="toggleOpen" class="lg:hidden">
+          <button id="toggleOpen" className="lg:hidden">
             <svg
-              class="w-7 h-7"
+              className="w-7 h-7"
               fill="#000"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
